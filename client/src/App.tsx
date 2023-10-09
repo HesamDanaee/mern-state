c65e2e3f7a59d73b09e21dc2da5902a146e532cc
Hesam Danaee <hesamdanaey@outlook.com>
2023-10-09 10:15:52 +0330

    add pages and alias to config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,25 @@
+// Globals
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Components
+import Home from "@/pages/Home";
+import Signin from "@/pages/SignIn";
+import SignUp from "@/pages/SignUp";
+import About from "@/pages/About";
+import Profile from "@/pages/Profile";
+
 function App() {
-  return <main></main>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import alias from "@rollup/plugin-alias";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), alias()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+  },
+});
